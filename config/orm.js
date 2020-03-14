06c1e864f7c6e9fbd0d38925972f377705e9fb0e
felixcloud6288 <mweston6288@knights.ucf.edu>
2020-03-14 17:07:56 -0400

    Created orm.js. Template for it is made but methods need to be filled in

---
+++ config/orm.js
@@ -0,0 +1,15 @@
+// Creates the calls to the SQL database
+
+const connection = require ("connection.js");
+
+
+const orm = {
+    // Get all items from the database
+    selectAll:function(){},
+    // add one item to the database
+    insertOne:function(){},
+    // edit one item from the database
+    updateOne:function(){}
+}
+
+module.exports = orm;
