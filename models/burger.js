619865aa47bce7782ad56c3f4afd08deab02d6e7
felixcloud6288 <mweston6288@knights.ucf.edu>
2020-03-15 15:59:13 -0400

    burger.js methods made

--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,29 @@
+// The go between for the app and database. Contains methods for editing, getting, and deleting elements in the database
+
 const orm = require("../config/orm.js");
 
 const burger = {
-    
+    // Get all items from the burgers database
+    // Takes a callback function
+    all: function(cb){
+        orm.selectAll("burgers", function(res){
+            cb(res);
+        })
+    },
+    // add one new element to the burger database
+    // Takes a database column, the name of the new burger and a callback function
+    add: function(col, burgerName, cb){
+        orm.insertOne("burgers", col, burgerName, (res)=>{
+            cb(res);
+        })
+    },
+    // change one element from the database
+    // Takes an existing burger object, the condition to change and a callback function
+    update: function(object, condition, cb){
+        orm.updateOne("burgers", object, condition, (res)=>{
+            cb(res)
+        })
+    }
 }
 
 module.exports = burger;
